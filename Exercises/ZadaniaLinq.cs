071d58f0c7da115d554e08ff365ad5282107d408
Damian Walczuk <05damiankv05@gmail.com>
2026-03-31 22:03:13 +0200

    implement Zadanie01

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -16,7 +16,11 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie01_StudenciZWarszawy()
     {
-        throw Niezaimplementowano(nameof(Zadanie01_StudenciZWarszawy));
+        var studentsFromWarsaw = DaneUczelni.Studenci
+            .Where(s => s.Miasto == "Warsaw")
+            .Select(s => $"{s.NumerIndeksu} {s.Imie} {s.Nazwisko} {s.Miasto}");
+
+        return studentsFromWarsaw;
     }
 
     /// <summary>
